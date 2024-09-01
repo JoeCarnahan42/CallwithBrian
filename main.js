adfc1bae23aa3b14668d27486aa03f2e772d06ce
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-09-01 12:10:05 -0500

    code question

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log([1, 2, 3] + [4, 5, 6]) // torilla
+function largestProduct(array) {
+    let prodArray = []
+    let largestProd = 0
+    for (let i = 0; i < array.length; i++) {
+        prodArray.push(array[i] *= array[i + 1])
+    }
+    prodArray.forEach((element) => {
+        if (element > largestProd) {
+            largestProd = element
+        }
+    })
+    return largestProd
+}    
+
+console.log(largestProduct([1, 3, 5, 8]))
